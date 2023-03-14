857ed1ad94fffa88ff97971df985dd395f3e0c26
Enjae Antonio <Enjaeantonio@outlook.com>
2023-03-13 20:51:42 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CodeFirstMusicSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
