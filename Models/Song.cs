deef317a666c684bb72b579b2aee4fe57f38e70e
Enjae Antonio <Enjaeantonio@outlook.com>
2023-03-19 13:39:24 -0500

    errors

--- Models/Song.cs
+++ Models/Song.cs
@@ -1,25 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace CodeFirstMusicSystem.Models
+﻿namespace CodeFirstMusicSystem.Models
 {
     public class Song
     {
         public int Id { get; set; }
-        public int TrackNumber { get; set; }
-
-        [Required(ErrorMessage = "Title is required.")]
-        [StringLength(100, ErrorMessage = "Title must be between 1 and 100 characters.", MinimumLength = 1)]
         public string Title { get; set; }
-
-        [Required(ErrorMessage = "Duration is required.")]
-        [Range(1, int.MaxValue, ErrorMessage = "Duration must be a positive number.")]
         public int DurationSeconds { get; set; }
-
-        [Required(ErrorMessage = "Album is required.")]
+        public int TrackNumber { get; set; }
         public virtual Album Album { get; set; }
         public int AlbumId { get; set; }
-
-        [Required(ErrorMessage = "Artist is required.")]
         public virtual ICollection<SongContributor> SongContributors { get; set; } = new HashSet<SongContributor>();
         public virtual ICollection<PlaylistSong> PlaylistSongs { get; set; } = new HashSet<PlaylistSong>();
 
